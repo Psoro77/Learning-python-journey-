b22ced331eb1ee546b849b59c766e862f93bd73b
psoro77 <psoro288@gmail.com>
2025-06-14 21:46:33 -0400

    more exercice

--- forloop.py
+++ forloop.py
@@ -68,6 +68,10 @@ listest = ['banane', 'pomme', 'poire', 'fraise', 'pineaple']
 #     prime= False
 #     for k in range(1, j):
 #         if ((j%k ==0 and (k==j or k==1)) or not(j%k==0)):
+## explication: si le nombre est divisible par k  et que le diviseur est 1 ou lui meme  c'est bon  on continue
+## de checker aussi si le nombre ne le divisise pas on continuer a checker a la fin si y'as aucun chiffre qui active
+##sa on passe dans is prime le cas ou c'est faux c'est quand un nombre le divise et que il est pas 1 ou lui 
+## or not devien dans ce cas False sa fait False False 
 #             prime=True 
 #         else : 
 #             prime=False
@@ -113,7 +117,82 @@ listest = ['banane', 'pomme', 'poire', 'fraise', 'pineaple']
 		
 # 	}
 # }
-   
+#array part
+#
+listnum = [22,21,1,2,3,4,99,56,7837,5,773,3763,892073,77,1,2,77,8272,99,5,5]
+# for nombre in listnum :
+#     count =0
+#     for i in range(len(listnum)):
+#         if nombre==listnum[i]:
+#             count=count+1
+#     print(f"{nombre} is counted {count} times")
+#ma version 
+#la version chatgptienne:
+#on utilise un dictionnaire ou notre nombre et la cle et le truc est la frequence si on croise on update la frequence en fesant 
+#plus un
+# freq ={}
+# for num in listnum:
+#     if num in freq :
+#         freq[num]+=1
+#     else:
+#         freq[num]=1
+# for number, frequence in freq.items():
+#     print(f"{number} appears {frequence} time")
+# recherche element
+#
+# elemnt =5
+# i=0
+# for number in listnum :
+
+#     if number==elemnt :
+#         print(f"element found at index : {i}")
+#     i+=1
+#supprimmer les doublons
+# freq = {}
+# for number in listnum:
+#     if number in freq :
+#         freq[number]+=1
+#     else:
+#         freq[number]=1 # je met un dictionnaire avec la frequence de chaque chiffre de la liste 
+
+# for cle, values in freq.items() :# je check si la veuleur est superieur a un je suprimme de la liste  pour quil en reste 
+#     #qu'un seul
+#     if values >1    :
+#         for i in range(values-1):
+#             listnum.remove(cle)
+# print(listnum)
+#pop enleve par rapport a l'index remove enleve l'occurence de la valeur dans la liste
+# programme pour demander un array et le decouper en deux
+redo = True
+listenc= []
+def stoparr(listen):
+    half= len(listen)/2
+    half =int(half)
+    listenc2 = listen[half:]
+    listen= listen[:half]
+    print(listen)
+    print(listenc2)
+    return
+while redo:
+    print('please enter a value |S to stop')
+    sc =input()
+    if sc ==('s' or 'S'):
+        redo= False
+        stoparr(listenc)
+        break
+    try :
+        nombre =int(sc)
+        listenc.append(nombre)
+        redo=True
+    except ValueError :
+        print('this is not an integer')
+
+
+
+    
+ 
+
+
         
 
  
